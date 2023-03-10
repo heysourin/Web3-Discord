fec2c98e4296d176a88d6795a9ccc2427d54a35b
heysourin <sourin07@gmail.com>
2023-03-10 23:50:24 +0530

    Updated

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
+
+const RPC_HTTP_URL = process.env.RPC_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: RPC_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 80001,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,36 @@
+const { ethers, run, network } = require("hardhat");
+//run allows to run any hardhat test
+
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory("Dappcord");
+  console.log("Deploying contract...");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+
+  console.log(`Contract deployed to ${simpleStorage.address}`);
+  if (network.config.chainId === 80001 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(1);
+    await verify(simpleStorage.address, []);
+  }
+}
+
+const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!")
+    } else {
+      console.log(e)
+    }
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
